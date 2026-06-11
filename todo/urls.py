f8ce9e3503182e75e13a1afdf18514f54b39907d
tayza <tayzafeature@gmail.com>
2026-06-11 22:25:07 +0700

    Fix 500 error: Correct delete_task view logic and JSON response

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     #(ဘယ် task_id လဲဆိုတာ ကိန်းကဏန်အနေနဲ့ ယူမယ်လို့ သတ်မှတ်တာပါ)
-    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete_task'),
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404 #redirect ကိုပါ import ထည့်ရမယ်
 from .models import Task 
+from django.views.decorators.csrf import csrf_exempt # လိုအပ်ရင်သုံးဖို့
 
 
 def home(request):
